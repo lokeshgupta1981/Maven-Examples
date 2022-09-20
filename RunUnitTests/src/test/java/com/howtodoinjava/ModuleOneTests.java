390040a8d9bebf3c54fe88d41302850049833573
lokeshgupta1981 <howtodoinjava@gmail.com>
2022-09-20 10:24:54 +0530

    Maven Junit Tests run

---
+++ RunUnitTests/src/test/java/com/howtodoinjava/ModuleOneTests.java
@@ -0,0 +1,25 @@
+package com.howtodoinjava;
+
+
+import static org.junit.Assert.assertTrue;
+
+import lombok.extern.java.Log;
+import org.junit.Test;
+
+@Log
+public class ModuleOneTests
+{
+    @Test
+    public void testMethodOne()
+    {
+        log.info("Executing ModuleOneTests#testMethodOne");
+        assertTrue( true );
+    }
+
+    @Test
+    public void testMethodTwo()
+    {
+        log.info("Executing ModuleOneTests#testMethodTwo");
+        assertTrue( false );
+    }
+}
