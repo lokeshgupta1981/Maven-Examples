249d7a0fb44ec1ee967cc6384d56ccfdd661a52e
lokeshgupta1981 <howtodoinjava@gmail.com>
2022-09-20 15:35:34 +0530

    Maven Junit Tests run

---
+++ RunUnitTests/README.md
@@ -0,0 +1,3 @@
+# Related Tutorials
+
+1. [How to Run JUnit Unit Tests with Maven](https://howtodoinjava.com/maven/maven-run-junit-tests/)
---
+++ RunUnitTests/src/test/java/com/howtodoinjava/ModuleTwoTests.java
@@ -0,0 +1,37 @@
+package com.howtodoinjava;
+
+import static org.junit.Assert.assertTrue;
+
+import lombok.extern.java.Log;
+import org.junit.Test;
+
+@Log
+public class ModuleTwoTests {
+  @Test
+  public void testMethodOne()
+  {
+    log.info("Executing ModuleTwoTests#testMethodOne");
+    assertTrue( false );
+  }
+
+  @Test
+  public void testMethodTwo()
+  {
+    log.info("Executing ModuleTwoTests#testMethodTwo");
+    assertTrue( true );
+  }
+
+  @Test
+  public void testMethodThree()
+  {
+    log.info("Executing ModuleTwoTests#testMethodThree");
+    assertTrue( true );
+  }
+
+  @Test
+  public void testMethodFour()
+  {
+    log.info("Executing ModuleTwoTests#testMethodFour");
+    assertTrue( true );
+  }
+}
